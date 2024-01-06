881b8d9c5faf94aaf12a59595b530be2c4ad122f
Harunradonic <harunradonic2016@gmail.com>
2024-01-06 22:54:01 +0100

    zadatak 4

--- app.js
+++ app.js
@@ -11,3 +11,15 @@ if (number > 0) {
 } else {
   console.log("Broj 8 nije veći od 0");
 }
+
+//zadatak 4
+const temp = [1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 6];
+
+if (temp[0] > 10) {
+  console.log(temp[0]);
+} else {
+  console.log(temp[19] * 10);
+}
+
+//Bug u kodu je bio u console.log(temp[20] * 10). 
+//20 ne postoji u nizu temp jer niz ima 20 elemenata.
