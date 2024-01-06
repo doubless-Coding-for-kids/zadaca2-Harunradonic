aa57660c4668e9201e611d73bafb1d6010f44218
Harunradonic <harunradonic2016@gmail.com>
2024-01-06 22:51:07 +0100

    zadatak 3

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
+//zadatak 2
 let pozdraviNiz = [zdravo, zdravo, zdravo, zdravo, zdravo, zdravo, zdravo, zdravo,];
 
 console.log(pozdraviNiz[7]);
+
+//zadatak 3
+let number = 8;
+
+if (number > 0) {
+  console.log("Broj 8 je veći od 0");
+} else {
+  console.log("Broj 8 nije veći od 0");
+}
